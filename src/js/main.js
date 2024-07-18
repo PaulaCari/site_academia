8a1c15580ce9c94769655ed6cb04b98f527a90d0
Paula Cari <paulola0223@gmail.com>
2024-07-18 16:27:20 -0300

    adicione home, js, variaveis

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+//Menu de nav movil responsivo
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar')
+// console.log(menu,navbar)
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-bars');
+    navbar.classList.toggle('active');
+};
