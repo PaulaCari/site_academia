6cf5d1d279ba58400ae12afd5b95e4d445763d7e
Paula Cari <paulola0223@gmail.com>
2024-07-20 10:08:39 -0300

    adicine responsividade

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 //Menu de nav movil responsivo
 let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar')
+let navbar = document.querySelector('.header .navbar');
 // console.log(menu,navbar)
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-bars');
     navbar.classList.toggle('active');
 };
+window
+
+// slider
