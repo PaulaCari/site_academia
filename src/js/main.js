be24a1a0962d9f6651969c5867ace8a0317ebd72
Paula Cari <paulola0223@gmail.com>
2024-07-24 10:21:17 -0300

    adicione o cogigo para os slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-//Menu de nav movil responsivo
+//hamburger
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 // console.log(menu,navbar)
@@ -7,6 +7,69 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-bars');
     navbar.classList.toggle('active');
 };
-window
+window.onscroll = () => {
+    menu.classList.remove('fa-bars');
+    navbar.classList.remove('active');
+};
 
-// slider
+// slides carrosel home
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 2000,  //velocidade slide
+    }, 
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+// console.log(swiper)
+
+// Feedbacks
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 3500, 
+        desibleOnInteraction: false, 
+    }, 
+    breakpoints:{
+        0: {
+            slidesPreView:1,
+        },
+        600:{
+            slidesPreView:2,
+        },
+    },
+ });
+
+//  blogs
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
